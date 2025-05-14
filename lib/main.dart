97f383f5e372cde0de26be14f19bd6274f11abe2
Heba <hebaelturk@gmail.com>
2025-05-14 23:30:09 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/cubits/add_note_cubit.dart';
 import 'package:notesapp/models/note_model.dart';
+import 'package:notesapp/simple_bloc_observer.dart';
 import 'package:notesapp/views/notes_view.dart';
 import 'package:notesapp/widgets/constants.dart';
 
 void main() async {
+
+  Bloc.observer = SimpleBlocObserver() ;
   await Hive.initFlutter();
 //وهنا بقولها يا هايف افتحي بوكس خزني فيه البيانات زي الكوليكشن ف البوست مان
  await Hive.openBox(kNotesBox);
